4f0dc0d526d69456f1be45c44abcfd9d8bbe8f50
Alexander Delos Santos <delos.santos.zabat.alexander@gmail.com>
2015-08-08 17:29:09 +0800

    Create post activity

--- app/src/main/java/com/photochop/photochop/CreatePostActivity.java
+++ app/src/main/java/com/photochop/photochop/CreatePostActivity.java
@@ -1,18 +1,41 @@
 package com.photochop.photochop;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-//import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentActivity;
+import android.widget.ImageView;
 
 
 /**
  * Created by alex on 8/8/15.
  */
-public class CreatePostActivity extends Activity {
+public class CreatePostActivity extends FragmentActivity {
+
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_post);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        image = (ImageView) findViewById(R.id.image);
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                handleSendImage(intent);
+            }
+        }
     }
 
+    void handleSendImage(Intent intent) {
+        Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (imageUri != null) {
+            image.setImageURI(imageUri);
+        }
+    }
 }
