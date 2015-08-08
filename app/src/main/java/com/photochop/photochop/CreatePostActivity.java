2814c73efac1d57b7cf99f26b2170f17791bc454
Alexander Delos Santos <delos.santos.zabat.alexander@gmail.com>
2015-08-08 15:22:13 +0800

    test

---
+++ app/src/main/java/com/photochop/photochop/CreatePostActivity.java
@@ -0,0 +1,18 @@
+package com.photochop.photochop;
+
+import android.app.Activity;
+import android.os.Bundle;
+//import android.support.v4.app.FragmentActivity;
+
+
+/**
+ * Created by alex on 8/8/15.
+ */
+public class CreatePostActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
